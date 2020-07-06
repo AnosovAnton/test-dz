e0075adb856b82a644e3cc4210bf93253cb5ed59
AnosovAnton <spector.rebop@mail.ru>
2020-07-07 01:55:26 +0300

    dz 3lesson

--- 3l_AnosovAnton.playground/Contents.swift
+++ 3l_AnosovAnton.playground/Contents.swift
@@ -1,138 +1,167 @@
 import UIKit
 
-enum act {
-    case workEng, doesWorkEng, openW, closeW, loadingC, unloadingC
-    }
-
-struct sportCar {
-    let pneumo: Bool
-    var model: String
-    let year: Int
-    var vVolume: Double
-    var closeWindows: act
-    var fillUpTheVolume: act
-    var workEngin: act
-    
-    mutating func cardoesWorkEngins() {
-        self.workEngin = .doesWorkEng
-    }
-    mutating func carWorkEngins() {
-        self.workEngin = .workEng
-    }
-    mutating func carsCloseWindows() {
-        self.closeWindows = .closeW
-    }
-    mutating func carsOpenWindows() {
-        self.closeWindows = .openW
-    }
-    var actSportCar: act {
-        willSet {
-            if newValue  == .closeW {
-                print("Окна закрыты")
-            }
-            else if newValue == .doesWorkEng {
-                print("Не работающий двигатель")
-            }
-            else if newValue == .loadingC {
-                print("Загруженный багажник")
-            }
-            else if newValue  == .openW {
-                print("Окна открыты")
-            }
-            else if newValue == .workEng {
-                print("Работающий двигатель")
-            }
-            else if newValue == .unloadingC {
-                print("Выгруженный багажник")
-            }
-            else if pneumo == true {
-                print("Есть пневматическая подвеска")
-            }
-            else {
-                print("Нет подвески")
-            }
-        }
-    }
-
-            init( model: String, year: Int, vVolume: Double, closeWindows: act, fillUpTheVolume: act, workEngin: act, pneumo: Bool) {
-                self.model = model
-                self.year = year
-                self.vVolume = vVolume
-                self.closeWindows = closeWindows
-                self.fillUpTheVolume = fillUpTheVolume
-                self.workEngin = workEngin
-                self.pneumo = pneumo
-    }
-    
-    func resultSpotCar(){
-    print("авто \(model) , год выпуска \(year), объем багажника \(vVolume), окна \(closeWindows), загруженный багажник \(fillUpTheVolume), двигатель \(workEngin), подвеска \(pneumo)")
-    }
+enum CarAction {
+    case engineOn, engineOff, openWindows, closeWindows
+    case cargoLoading(weight: Int)
+    case cargoUnloading(weight: Int)
 }
-
-struct truckCar {
-    var trailer: Bool
-    var model: String
+struct SportCar {
+    let model: String
     let year: Int
-    var vVolume: Double // объем багажника
-    var closeWindows: act
-    var fillUpTheVolume: act // заполненый объем багажника
-    var workEngin: act
+    let engineWork: Bool
+    let openWindows: Bool
+    let vVolume: Double
+    let fillTheVolume: Double
+    let pneumo: Bool
     
-        mutating func cardoesWorkEngins() {
-               self.workEngin = .doesWorkEng
-        }
-        mutating func carWorkEngins() {
-               self.workEngin = .workEng
-        }
-        mutating func carsCloseWindows() {
-            self.closeWindows = .closeW
-        }
-        mutating func carsOpenWindows() {
-            self.closeWindows = .openW
-        }
-    
-    var actTruckCar: act {
-        willSet {
-            if newValue  == .closeW {
-                print("Окна закрыты")
-            }
-            else if newValue == .doesWorkEng {
-                print("Не работающий двигатель")
-            }
-            else if newValue == .loadingC {
-                print("Загруженный багажник")
-            }
-            else if newValue  == .openW {
-                print("Окна открыты")
-            }
-            else if newValue == .workEng {
-                print("Работающий двигатель")
-            }
-            else if newValue == .unloadingC {
-                print("Выгруженный багажник")
-            }
-            else if trailer == true {
-                print("Есть прицеп")
-            }
-            else {
-                print("Прицепа нет")
+        mutating func carAction (action: CarAction) {
+            switch action {
+            case .engineOn:
+                if engineWork == true {
+                   print("Двигатель работает")
+                }
+                else {
+                    print("Двигатель не работает")
+                }
+            case .openWindows:
+                if openWindows == true {
+                    print("Окна открыты")
+                }
+                else {
+                    print("Окна закрыты")
+                }
+                
+            case .engineOff:
+                if engineWork == true {
+                    print("Двигатель не работает")
+                }
+                else {
+                    print("Двигатель работает")
+                }
+            case .closeWindows:
+                if openWindows == true {
+                    print("Окна закрыты")
+                }
+                else {
+                    print("Окна открыты")
+                }
+            case .cargoLoading(weight: _):
+                if fillTheVolume < vVolume {
+                    print("Нельзя загрузить")
+                }
+                else {
+                    print("груз загружен")
+                }
+            case .cargoUnloading(weight: _):
+                if vVolume < fillTheVolume {
+                    print("Грузы выгружен")
+                }
+                else {
+                    print("груз не выгружен")
+                }
+                if pneumo == true {
+                    print("Имеет пневмоподвеску")
+                }
+                else {
+                    print("не имеет подвески")
                 }
             }
+            
+        }
+
+        init(model: String, year: Int, engineWork: Bool, openWindows: Bool, vVolume: Double, fillTheVolume: Double, pneumo: Bool) {
+        self.model = model
+        self.year = year
+        self.engineWork = false
+        self.openWindows = false
+        self.vVolume = 3
+        self.fillTheVolume = 10
+        self.pneumo = true
     }
+    func resultSportCar () {
+        print("марка автомобиля \(model), год выпуска \(year), двигатель \(engineWork), окна \(openWindows), объем багажника \(vVolume), загрузка багажника \(fillTheVolume), подвеска \(pneumo)")
+    }
+    }
+
+    struct TruckCar {
+          let model: String
+          let year: Int
+          let engineWork: Bool
+          let openWindows: Bool
+          let vVolume: Double
+          let fillTheVolume: Double
+          let trailer: Bool
         
-        init( model: String, year: Int, vVolume: Double, closeWindows: act, fillUpTheVolume: act, workEngin: act, trailer: Bool) {
+        mutating func carAction (action: CarAction) {
+                switch action {
+                case .engineOn:
+                    if engineWork == true {
+                       print("Двигатель работает")
+                    }
+                    else {
+                        print("Двигатель не работает")
+                    }
+                case .openWindows:
+                    if openWindows == true {
+                        print("Окна открыты")
+                    }
+                    else {
+                        print("Окна закрыты")
+                    }
+                    
+                case .engineOff:
+                    if engineWork == true {
+                        print("Двигатель не работает")
+                    }
+                    else {
+                        print("Двигатель работает")
+                    }
+                case .closeWindows:
+                    if openWindows == true {
+                        print("Окна закрыты")
+                    }
+                    else {
+                        print("Окна открыты")
+                    }
+                case .cargoLoading(weight: _):
+                    if fillTheVolume < vVolume {
+                        print("Нельзя загрузить")
+                    }
+                    else {
+                        print("груз загружен")
+                    }
+                case .cargoUnloading(weight: _):
+                    if vVolume < fillTheVolume {
+                        print("Грузы выгружен")
+                    }
+                    else {
+                        print("груз не выгружен")
+                    }
+                    if trailer == true {
+                        print("Имеет  прицеп")
+                    }
+                    else {
+                        print("не имеет прицепа")
+                    }
+                }
+                
+            }
+
+            init(model: String, year: Int, engineWork: Bool, openWindows: Bool, vVolume: Double, fillTheVolume: Double, trailer: Bool) {
             self.model = model
             self.year = year
-            self.vVolume = vVolume
-            self.closeWindows = closeWindows
-            self.fillUpTheVolume = fillUpTheVolume
-            self.workEngin = workEngin
+            self.engineWork = engineWork
+            self.openWindows = openWindows
+            self.vVolume = 20
+            self.fillTheVolume = 10
             self.trailer = trailer
-    }
-    func resultTruckCar(){
-        print("авто \(model) , год выпуска \(year), объем багажника \(vVolume), окна \(closeWindows), загруженный багажник \(fillUpTheVolume), двигатель \(workEngin), прицеп \(trailer)")
-    }
-}
+        }
+        func resultTruckCar () {
+            print("марка автомобиля \(model), год выпуска \(year), двигатель \(engineWork), окна \(openWindows), объем кузова \(vVolume), загрузка кузова \(fillTheVolume), прицеп \(trailer)")
+        }
+        }
+var car1 = SportCar(model: "Volvo", year: 2020, engineWork: true, openWindows: false, vVolume: 3, fillTheVolume: 2.5, pneumo: true)
+car1.resultSportCar()
+var car2 = TruckCar(model: "Audi", year: 2010, engineWork: false, openWindows: true, vVolume: 80, fillTheVolume: 60, trailer: false)
+car2.resultTruckCar()
 
-var car1 = sportCar(model: "Vovo", year: 2020, vVolume: 5, closeWindows: .closeW, fillUpTheVolume: .unloadingC, workEngin: .workEng, pneumo: true)
-   
