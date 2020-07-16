df2aeeaeedf60d0ab1d5df9a702afbd5a8525891
AnosovAnton <spector.rebop@mail.ru>
2020-07-17 00:16:56 +0300

    обновленный1

--- l6_AnosovAnton.playground/Contents.swift
+++ l6_AnosovAnton.playground/Contents.swift
@@ -1,38 +1,87 @@
 import UIKit
-class Car {
-    var weight: Double
-    var long: Int
-        
-        init(weight: Double, long: Int) {
-            self.weight = weight
-            self.long = long
-        }
-}
-class TruckCar {
-        var weight: Double
-    var long: Int
-        
-        init(weight: Double, long: Int) {
-            self.weight = weight
-            self.long = long
-        }
-}
+
+ 
     
+    class Car {
+    var weight: Double
+    var  carfenderWeight: Double
+    var carHood: Int
+    var aDoorWeight: Double
+    
+        func totalWeight () -> Double {
+            return carfenderWeight + Double(carHood) + aDoorWeight
+        }
+        
+        init(carfenderWeight: Double, carHood: Int, aDoorWeight: Double, weight: Double) {
+            self.carfenderWeight = carfenderWeight
+            self.carHood = carHood
+            self.aDoorWeight = aDoorWeight
+            self.weight = weight
+        }
+}
+    class TruckCar {
+    var  carfenderWeight: Double
+    var carHood: Int
+    var aDoorWeight: Double
+    var weight: Double
+        
+        func totalWeight () -> Double {
+            return carfenderWeight + Double(carHood) + aDoorWeight
+        }
+        
+        init(carfenderWeight: Double, carHood: Int, aDoorWeight: Double, weight: Double) {
+            self.carfenderWeight = carfenderWeight
+            self.carHood = carHood
+            self.aDoorWeight = aDoorWeight
+            self.weight = weight
+        }
+
+}
 
 struct Queue<T> {                   // T - это какой-то пока неизвестный тип
-    private var elements: [T] = []           // массив типа Т
+    private var elements: [T] = []
+    // массив типа Т
     mutating func push(_ element: T) {      // добавляем элемент Т
         elements.append(element)
         }
     mutating func pop() -> T? {           // извлекаем элемент типа Т
          return elements.removeLast()
         }
-    }
+    
+     func filter(_ predicate: (T) -> Bool ) -> [T] {
+            var result = [T]()
+            for item in elements {
+                if predicate(item) {
+                    result.append(item)
+                }
+            }
+            return result
+        }
+}
 
 var queuekCar = Queue<Car> ()
 var queueTruckCar = Queue<TruckCar> ()
 
-queuekCar.push(Car(weight: 2.45, long: 4))
-queuekCar.push(Car(weight: 3.45, long: 5))
-queueTruckCar.push(TruckCar(weight: 1355.7, long: 12))
-queueTruckCar.push(TruckCar(weight: 1255.7, long: 17))
+queuekCar.push(Car(carfenderWeight: 5.3, carHood: 2, aDoorWeight: 5.2, weight: 1000))
+queuekCar.push(Car(carfenderWeight: 3.2, carHood: 4, aDoorWeight: 4.8, weight: 1100))
+queueTruckCar.push(TruckCar(carfenderWeight: 10.1, carHood: 5, aDoorWeight: 7.3, weight: 3000))
+queueTruckCar.push(TruckCar(carfenderWeight: 9.2, carHood: 10, aDoorWeight: 7.8, weight: 5000))
+print(queuekCar)
+
+queuekCar.pop ()
+print(queuekCar)
+
+
+
+
+var queue = Queue<Int>()
+queue.push(23)
+queue.push(2)
+queue.push(45)
+queue.push(46)
+
+print(queue)
+
+print(queue.filter {
+    $0 > 23
+})
